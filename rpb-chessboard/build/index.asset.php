9f90e9461f63cb6dba24632d686c73163420155b
Sean Murphy <sean@iamseanmurphy.com>
2026-07-18 15:11:48 +0000

    8.1.5

--- rpb-chessboard/build/index.asset.php
+++ rpb-chessboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-primitives'), 'version' => '2e985b53123792e60f51');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-primitives'), 'version' => 'ccf6dc6a207a3e4f219c');
